03009f7d7f56dc2a3aba4f28a1fbc68a2544e210
nattscholz <natalyscholzperaza09@gmail.com>
2026-05-20 20:10:14 -0600

    Commit #1
    Creación del proyecto, semana #1 y #2

---
+++ src/test/java/TechShop/NatalyScholz/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.NatalyScholz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
